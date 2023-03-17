bbc61fb2661bfd40680835d4a58e43f35ba348df
Patricio Quishpe <109158582+PatricioInsider@users.noreply.github.com>
2023-03-16 22:02:29 -0500

    kind of matriz with process

--- ejer-intermedio/L5-matriz.py
+++ ejer-intermedio/L5-matriz.py
@@ -1,8 +1,17 @@
 def max_fila(m1):
+    """programa que lea una matriz 3 x 3 y devuelva el máximo de cada fila
+
+    Args:
+        m1 (int): matriz ingresada que se evalua  
+
+    Returns:
+        maximos: lista que contiene el numero mayor de cada fila de la matriz ingresada
+    """
+    
     maximos = []
     
     for fila in m1:
-        aux = min(fila)
+        aux = max(fila) #obtiene el maximo de la fila, y si lo cambiamos por min, obtenemos el minimo de la misma 
         maximos.append(aux)
     return maximos
 
