c075f2cab310a1fd6c6b04c4ca87eb92566a14dc
Patricio Quishpe <109158582+PatricioInsider@users.noreply.github.com>
2023-03-20 23:27:30 -0500

    The filter() function that works with lambda was added to the theory

--- ejer-intermedio/Funciones-Lambda.py
+++ ejer-intermedio/Funciones-Lambda.py
@@ -24,11 +24,29 @@ def basico2():
     prod = lambda x,y:x+y
     print (prod(5,10))
     
-def discriminante():
-    discri = lambda a,b,c : pow(b,2)- 4*(a*c)
-    print (discri(1,2,1)) 
+def basico3():
+    discriminante = lambda a,b,c : pow(b,2)- 4*(a*c)
+    print (discriminante(1,2,1)) 
+    
+def basico4():
+    """crear una tupla con un numero ingresado, que guarde el numero, su doble, y su exponente
+    """
+    double_square = lambda x:(x,2*x,x**2)
+    print(double_square(3))    
+    
+"""
+La función filter()
+
+Aplica una función a todos los elementos de un objeto iterable
+Devuelve un objeto generador, de ahí que usemos la función list() para convertirlo a lista
+Como output, devuelve los elementos para los cuales el aplicar la función ha devuelto True
+Con la ayuda de las funciones lambda, apliquemos filter() para quedarnos con los números múltiplos de 7 de la siguiente lista llamada nums
+    """
+def fun_filter():
+    nums = [49, 57,62,147,2101,22]
+    print(int(filter(lambda x: (x%7 ==0), nums)))
     
 
 if __name__ == "__main__":
-    discriminante()
+    fun_filter()
 
