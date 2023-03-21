2c96741269ced15124898d991684c50dc61bca25
Patricio Quishpe <109158582+PatricioInsider@users.noreply.github.com>
2023-03-20 23:29:57 -0500

    the filter() function was added

--- ejer-intermedio/Funciones-Lambda.py
+++ ejer-intermedio/Funciones-Lambda.py
@@ -44,8 +44,16 @@ Con la ayuda de las funciones lambda, apliquemos filter() para quedarnos con los
     """
 def fun_filter():
     nums = [49, 57,62,147,2101,22]
-    print(int(filter(lambda x: (x%7 ==0), nums)))
-    
+    print(list(filter(lambda x: (x%7 ==0), nums)))
+    #Filter requiere de dos parametros (funcion- recibe como parametro el elemento,lista - recoge el elemento)
+    #La funcion filter, devuelve objetos generadores(objetos iterables) que deben ser designados a una lista
+    """La función proporcionada a filter() no tiene por qué ser lambda, sino que puede ser una ya existente, o bien una creada por nosotros mismos.
+Con las siguientes líneas de código vamos a obtener todas las palabras cuya tercera letra sea s haciendo uso de filter() y la función creada third_letter_is_s():
+    """
+    def third_letter_is_s(word):
+        return word[2] == "s"
+    words = ["castaña", "astronomía", "masa", "bolígrafo", "mando", "tostada"]
+    list(filter(third_letter_is_s, words))
 
 if __name__ == "__main__":
     fun_filter()
